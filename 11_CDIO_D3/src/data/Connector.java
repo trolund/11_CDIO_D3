e36a3a54d86c6c0482ea5b75d1d622341047dac4
InAbsencia <absentia@protonmail.ch>
2017-03-20 20:10:45 +0100

    Implemented secure MySQL DB layer.

--- 11_CDIO_D3/src/data/Connector.java
+++ 11_CDIO_D3/src/data/Connector.java
@@ -1,23 +1,31 @@
 package data;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class Connector {
 
-    private final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
-    private final String DRIVER = "jdbc:mysql:";
-    private final String HOST = "Localhost";
-    private final int PORT = 3306;
-    private final String DATABASE = "02327_project";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "";
+    private final String CONFIG_FILE = "mysql.config";
+    private String driverClass;
+    private String driver;
+    private String host;
+    private int port;
+    private String database;
+    private String username;
+    private String password;
+
     private Connection connection;
 
     public Connector() {
+        loadProperties();
         try {
-            Class.forName(DRIVER_CLASS);
-            String url = DRIVER + "//" + HOST + ":" + PORT + "/" + DATABASE;
-            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+            Class.forName(driverClass);
+            String url = driver + "://" + host + ":" + port + "/" + database;
+            connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             System.exit(1);
@@ -36,6 +44,24 @@ public class Connector {
     public void doUpdate(String query) throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.executeUpdate(query);
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(CONFIG_FILE)) {
+            properties.load(is);
+            this.driverClass = properties.getProperty("DRIVER_CLASS");
+            this.driver = properties.getProperty("DRIVER");
+            this.host = properties.getProperty("HOST");
+            this.port = Integer.parseInt(properties.getProperty("PORT"));
+            this.database = properties.getProperty("DATABASE");
+            this.username = properties.getProperty("USERNAME");
+            this.password = properties.getProperty("PASSWORD");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
