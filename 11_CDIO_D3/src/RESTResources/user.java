fab45253b18ec0bcb16343deca229f2cc49172cf
trolund <troelslund@192.168.1.46>
2017-03-21 20:28:31 +0100

    ...

---
+++ 11_CDIO_D3/src/RESTResources/user.java
@@ -0,0 +1,14 @@
+package RESTResources;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+public class user {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String getUser(){
+		return "Troels Lund";
+	}
+
+}
