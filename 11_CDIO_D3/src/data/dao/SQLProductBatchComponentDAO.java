73a5efd904996056f73f83e2aec15227268d04c0
InAbsencia <absentia@protonmail.ch>
2017-03-22 21:06:34 +0100

    Implemented SQLProductBatchComponentDAO.

--- 11_CDIO_D3/src/data/dao/SQLProductBatchComponentDAO.java
+++ 11_CDIO_D3/src/data/dao/SQLProductBatchComponentDAO.java
@@ -1,14 +1,46 @@
 package data.dao;
 
+import data.Connector;
 import data.dto.ProductBatchComponentDTO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLProductBatchComponentDAO implements IProductBatchComponentDAO {
 
+    private final Connector connector;
+
+    public SQLProductBatchComponentDAO(Connector connector) {
+        this.connector = connector;
+    }
+
     @Override
     public ProductBatchComponentDTO getProductBatchComponent(int pbId, int rbId) throws DALException {
-        return null;
+        String getPBCSql = "SELECT * FROM produktbatchkomponent WHERE pb_id = ? AND rb_id = ?";
+        PreparedStatement getPBCStmt = null;
+        ResultSet rs = null;
+        try {
+            getPBCStmt = connector.getConnection().prepareStatement(getPBCSql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            getPBCStmt.setInt(1, pbId);
+            getPBCStmt.setInt(2, rbId);
+            rs = getPBCStmt.executeQuery();
+
+            if (!rs.first())
+                throw new DALException("ProductBatchComponent with pbId [" + pbId + "] and rbId [" + rbId + "] does not exist!");
+
+            return new ProductBatchComponentDTO(rs.getInt("pb_id"), rs.getInt("rb_id"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id"));
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage(), e);
+        } finally {
+            try {
+                connector.cleanup(getPBCStmt, rs);
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
@@ -18,17 +50,72 @@ public class SQLProductBatchComponentDAO implements IProductBatchComponentDAO {
 
     @Override
     public List<ProductBatchComponentDTO> getProductBatchComponentList() throws DALException {
-        return null;
+        String getPBCListSql = "SELECT * FROM produktbatchkomponent";
+        List<ProductBatchComponentDTO> pbcList = new ArrayList<>();
+        PreparedStatement getPBCListStmt = null;
+        ResultSet rs = null;
+        try {
+            getPBCListStmt = connector.getConnection().prepareStatement(getPBCListSql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            rs = getPBCListStmt.executeQuery();
+            while (rs.next()) {
+                pbcList.add(new ProductBatchComponentDTO(rs.getInt("rb_id"), rs.getInt("rb_id"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("opr_id")));
+            }
+            return pbcList;
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage(), e);
+        } finally {
+            try {
+                connector.cleanup(getPBCListStmt, rs);
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
     public void createProductBatchComponent(ProductBatchComponentDTO pbc) throws DALException {
-
+        String createPBCSql = "INSERT INTO produktbatchkomponent(pb_id, rb_id, tara, netto, opr_id) VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement createPBCStmt = null;
+        try {
+            createPBCStmt = connector.getConnection().prepareStatement(createPBCSql);
+            createPBCStmt.setInt(1, pbc.getpBId());
+            createPBCStmt.setInt(2, pbc.getrBId());
+            createPBCStmt.setDouble(3, pbc.getTara());
+            createPBCStmt.setDouble(4, pbc.getNetto());
+            createPBCStmt.setInt(5, pbc.getOprId());
+            createPBCStmt.executeQuery();
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage(), e);
+        } finally {
+            try {
+                connector.cleanup(createPBCStmt);
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
     public void updateProductBatchComponent(ProductBatchComponentDTO pbc) throws DALException {
-
+        String updatePBCSql = "UPDATE produktbatchkomponent SET tara = ?, netto = ? WHERE pb_id = ? AND rb_id = ? AND opr_id = ?";
+        PreparedStatement updatePBCStmt = null;
+        try {
+            updatePBCStmt = connector.getConnection().prepareStatement(updatePBCSql);
+            updatePBCStmt.setDouble(1, pbc.getTara());
+            updatePBCStmt.setDouble(2, pbc.getNetto());
+            updatePBCStmt.setInt(3, pbc.getpBId());
+            updatePBCStmt.setInt(4, pbc.getrBId());
+            updatePBCStmt.setInt(5, pbc.getOprId());
+            updatePBCStmt.executeQuery();
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage(), e);
+        } finally {
+            try {
+                connector.cleanup(updatePBCStmt);
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
--- 11_CDIO_D3/src/test/data/TestData.java
+++ 11_CDIO_D3/src/test/data/TestData.java
@@ -10,7 +10,9 @@ import data.Connector;
 import data.IData;
 import data.dao.DALException;
 import data.dao.SQLOperatorDAO;
+import data.dao.SQLProductBatchComponentDAO;
 import data.dto.OperatorDTO;
+import data.dto.ProductBatchComponentDTO;
 
 import java.util.List;
 
@@ -28,10 +30,9 @@ public class TestData implements IData {
         /* Testing SQLOperatorDAO.getOperator(oprId) */
         try {
             OperatorDTO oprDtoId1 = operatorDao.getOperator(1);
-            System.out.println("\n##### Testing SQLOperatorDAO.getOperator(oprId) #####");
-            System.out.println("##### Retrieving OperatorDTO from database...   #####\n");
+            System.out.println("\n##### Testing SQLOperatorDAO.getOperator(oprId) #####\n");
             System.out.println(oprDtoId1);
-            System.out.println("\n#####################################################\n");
+            System.out.println("\n#####################################################\n\n");
         } catch (DALException e) {
             e.printStackTrace();
         }
@@ -40,11 +41,10 @@ public class TestData implements IData {
         try {
             List<OperatorDTO> list = operatorDao.getOperatorList();
 
-            System.out.println("##### Testing SQLOperatorDAO.getOperatorList()  #####");
-            System.out.println("##### Retrieving OperatorDTO from database...   #####\n");
+            System.out.println("##### Testing SQLOperatorDAO.getOperatorList()  #####\n");
             for (int i = 0; i < list.size(); i++)
                 System.out.println(i + ": " + list.get(i));
-            System.out.println("\n#####################################################\n");
+            System.out.println("\n#####################################################\n\n");
 
         } catch (DALException e) {
             e.printStackTrace();
@@ -52,22 +52,69 @@ public class TestData implements IData {
 
         /* Testing SQLOperatorDAO.createOperator() */
         System.out.println("##### Testing SQLOperatorDAO.createOperator(oprDto) #####");
-        System.out.println("##### Creating Operator Arnold..................... #####");
         try {
             OperatorDTO oprDto = new OperatorDTO(666, "Arnold", "ASN", "234234", "hellokitty", null);
             operatorDao.createOperator(oprDto);
-            System.out.println("#####################################################\n");
+            System.out.println("#####################################################\n\n");
         } catch (DALException e) {
-            System.err.println(e.getMessage() + "\n");
+            System.out.println("\n" + e.getMessage() + "\n");
         }
 
         /* Testing SQLOperatorDAO.updateOperator() */
-        System.out.println("##### Testing SQLOperatorDAO.updateOperator(oprDto) #####");
-        System.out.println("##### Updating Operator Arnold..................... #####");
+        System.out.println("##### Testing SQLOperatorDAO.updateOperator(oprDto) #####\n");
         try {
             OperatorDTO oprDto = new OperatorDTO(666, "Arnold", "ASN", "234234", "hellokittyunderwear", null);
             operatorDao.updateOperator(oprDto);
-            System.out.println("#########################################################\n");
+            System.out.println(operatorDao.getOperator(666));
+            System.out.println("\n#########################################################\n\n");
+        } catch (DALException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testSQLProductBatchComponentDAO() {
+        SQLProductBatchComponentDAO SQLProductBatchCompDao = new SQLProductBatchComponentDAO(connector);
+
+        /* Testing SQLProductBatchComponentDAO.getProductBatchComponent(pbId, rbId) */
+        try {
+            System.out.println("##### Testing SQLProductBatchComponentDAO.getProductBatchComponent(pbId, rbId) #####\n");
+            ProductBatchComponentDTO pbcDTO = SQLProductBatchCompDao.getProductBatchComponent(1, 2);
+            System.out.println(pbcDTO);
+        } catch (DALException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n####################################################################################\n\n");
+
+        /* Testing SQLProductBatchComponentDAO.getProductBatchComponentList() */
+        try {
+            List<ProductBatchComponentDTO> list = SQLProductBatchCompDao.getProductBatchComponentList();
+            System.out.println("\n##### Testing SQLProductBatchComponentDAO.getProductBatchComponentList() #####\n");
+            for (int i = 0; i < list.size(); i++)
+                System.out.println(i + ": " + list.get(i));
+            System.out.println("\n#####################################################\n\n");
+        } catch (DALException e) {
+            e.printStackTrace();
+        }
+
+        /* Testing SQLProductBatchComponentDAO.createProductBatchComponent() */
+        System.out.println("\n##### Testing SQLProductBatchComponentDAO.createPBC(pbcDTO) #####");
+        try {
+            ProductBatchComponentDTO pbcDTO = new ProductBatchComponentDTO(1, 6, 20.5, 10.5, 1);
+            SQLProductBatchCompDao.createProductBatchComponent(pbcDTO);
+            System.out.println("Created: " + pbcDTO);
+            System.out.println("#####################################################\n\n");
+        } catch (DALException e) {
+            System.out.println("\n" + e.getMessage() + "\n");
+        }
+
+        /* Testing SQLProductBatchComponentDAO.updatePBC(pbcDTO) */
+        System.out.println("##### Testing SQLProductBatchComponentDAO.updateProductBatchComponent(pbcDto) #####\n");
+        try {
+            ProductBatchComponentDTO pbcDTO = new ProductBatchComponentDTO(4, 6, 22.2, 22.2, 3);
+            SQLProductBatchCompDao.updateProductBatchComponent(pbcDTO);
+            System.out.println(SQLProductBatchCompDao.getProductBatchComponent(4, 6));
+            System.out.println("\n#########################################################\n\n");
         } catch (DALException e) {
             e.printStackTrace();
         }
@@ -76,7 +123,11 @@ public class TestData implements IData {
 
     public static void main(String[] args) {
         TestData testData = new TestData();
+
+        System.out.println("\n################### Testing SQLOperatorDAO ###################");
         testData.testSQLoperatorDAO();
+        System.out.println("\n\n\n################### Testing SQLProductBatchComponentDAO ###################\n");
+        testData.testSQLProductBatchComponentDAO();
     }
 
 }
