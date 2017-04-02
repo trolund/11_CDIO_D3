e4cd59668bed610588e74c28278bf839131a192f
InAbsencia <absentia@protonmail.ch>
2017-04-02 13:00:39 +0200

    Maven now fully controls our used APIs. (IntelliJ support.)

--- 11_CDIO_D3/src/RESTResources/user.java
+++ 11_CDIO_D3/src/RESTResources/user.java
@@ -1,14 +1,15 @@
 package RESTResources;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
 
-//@Path("/user")
-//public class user {
-//
-//	@GET
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public String getUser(){
-//		return "Troels Lund";
-//	}
-//
-//}
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+public class user {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getUser() {
+        return "Troels Lund";
+    }
+
+}
