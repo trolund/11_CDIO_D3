3b1fe9384370aaaa599d044c73e4be0b90a8d5d5
InAbsencia <absentia@protonmail.ch>
2017-04-04 09:16:54 +0200

    Oops.

--- 11_CDIO_D3/src/utils/SecUtil.java
+++ 11_CDIO_D3/src/utils/SecUtil.java
@@ -10,7 +10,7 @@ public class SecUtil {
 
 	}
 
-	public static String sha256(String base) {
+	public String sha256(String base) {
 		try {
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
 			byte[] hash = digest.digest(base.getBytes("UTF-8"));
