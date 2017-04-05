9bcbf86930fc42082811a9a583ffda6a0fff85ba
InAbsencia <absentia@protonmail.ch>
2017-04-05 15:51:09 +0200

    Added inline Connector.java documentation.

--- 11_CDIO_D3/src/data/Connector.java
+++ 11_CDIO_D3/src/data/Connector.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.Properties;
 
 /*
- * Database connector class.
+ * Singleton database connector class.
  * 
  * This class has the responsibility to load database information and SQL queries 
  * as start up as well as to maintain them and update them accordingly.
@@ -24,14 +24,17 @@ import java.util.Properties;
 public class Connector {
 
 	/*
-	 * 
+	 * MYSQL_CONFIG_FILE: Path to the database configuration file.
+	 * SQL_CONFIG_FILE:   Path to the SQL query configuration file.
 	 */
 	private final String MYSQL_CONFIG_FILE = "/home/absencia/workspace/11_CDIO_D3/11_CDIO_D3/mysql_mariadb.config";
 	private final String SQL_CONFIG_FILE = "/home/absencia/workspace/11_CDIO_D3/11_CDIO_D3/sql.config";
-
 	//private final String MYSQL_CONFIG_FILE = "/Users/troelslund/git/11_CDIO_D3_2/11_CDIO_D3/mysql_oracle.config";
 	//private final String SQL_CONFIG_FILE = "/Users/troelslund/git/11_CDIO_D3_2/11_CDIO_D3/sql.config";
 
+	/*
+	 * String objects to hold the database configuration information.
+	 */
 	private String driverClass;
 	private String driver;
 	private String host;
@@ -40,11 +43,22 @@ public class Connector {
 	private String username;
 	private String password;
 
+	/*
+	 * Database connection object and HashMap object to store all 
+	 * SQL queries used by this application.
+	 */
 	private Connection connection;
 	private Map<String, String> sqlHashMap;
 
+	/*
+	 * Singleton instance of this class.
+	 */
 	private static final Connector instance = new Connector();
 
+	/*
+	 * Constructor that loads all of the configuration files at startup
+	 * and creates a database connection immediately.
+	 */
 	private Connector() {
 		sqlHashMap = new HashMap<>();
 		loadConnectorProperties();
@@ -59,9 +73,13 @@ public class Connector {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		System.out.println("DEBUG: [" + this.getClass().getName() + "] Status: Successfully connected to MySQL database,\nhost '" + host + "' with user '" + username + "'.");
+		System.out.println("DEBUG: [DBConnection] Status: Successfully connected to MySQL database,\nhost '" + host + "' with user '" + username + "'.");
 	}
 
+	/*
+	 * Method to load the database configuration from file
+	 * into Java properties.
+	 */
 	private void loadConnectorProperties() {
 		Properties p = new Properties();
 		try (InputStream is = new FileInputStream(MYSQL_CONFIG_FILE)) {
@@ -80,8 +98,12 @@ public class Connector {
 		}
 	}
 
+	/*
+	 * Method to create the SQL query configuration file.
+	 */
 	private void createSQLProperties() {
 		Properties p = new Properties();
+
 		// Operator SQL
 		p.setProperty("getOprSql", "SELECT * FROM operatoer WHERE opr_id = ?");
 		p.setProperty("getOprListSql", "SELECT * FROM operatoer");
@@ -140,6 +162,10 @@ public class Connector {
 		}
 	}
 
+	/*
+	 * Method to load the SQL query configuration file
+	 * and put all of the information into a HashMap.
+	 */
 	private void loadSQLProperties() {
 		Properties p = new Properties();
 		try (InputStream is = new FileInputStream(SQL_CONFIG_FILE)) {
@@ -155,27 +181,46 @@ public class Connector {
 		}
 	}
 
+	/*
+	 * Method to close a desired PreparedStatement
+	 * and ResultSet.
+	 */
 	public void cleanup(PreparedStatement stmt, ResultSet rs) throws SQLException {
 		if (stmt != null) stmt.close();
 		if (rs != null) rs.close();
 	}
 
+	/*
+	 * Method to close a desired PreparedStatement.
+	 */
 	public void cleanup(PreparedStatement stmt) throws SQLException {
 		if (stmt != null) stmt.close();
 	}
 
+	/*
+	 * Method to close the database connection.
+	 */
 	public void closeConnection() throws SQLException {
 		connection.close();
 	}
 
+	/*
+	 * Method to get the desired SQL query at the key.
+	 */
 	public String getSQL(String key) {
 		return sqlHashMap.get(key);
 	}
 
+	/*
+	 * Method to get the database connection object.
+	 */
 	public Connection getConnection() {
 		return connection;
 	}
 
+	/*
+	 * Method to return the Singleton instance of this class.
+	 */
 	public static synchronized Connector getInstance() {
 		return instance;
 	}
