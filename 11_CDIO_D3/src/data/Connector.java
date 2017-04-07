7abd485afcfff85ec6a5414adcabcdaa02c3a7bb
InAbsencia <absentia@protonmail.ch>
2017-04-07 21:37:34 +0200

    Cleaning up project structure.

--- 11_CDIO_D3/src/data/Connector.java
+++ 11_CDIO_D3/src/data/Connector.java
@@ -27,10 +27,8 @@ public class Connector {
 	 * MYSQL_CONFIG_FILE: Path to the database configuration file.
 	 * SQL_CONFIG_FILE:   Path to the SQL query configuration file.
 	 */
-	private final String MYSQL_CONFIG_FILE = "/home/absencia/workspace/11_CDIO_D3/11_CDIO_D3/mysql_mariadb.config";
-	private final String SQL_CONFIG_FILE = "/home/absencia/workspace/11_CDIO_D3/11_CDIO_D3/sql.config";
-	//private final String MYSQL_CONFIG_FILE = "/Users/troelslund/git/11_CDIO_D3_2/11_CDIO_D3/mysql_oracle.config";
-	//private final String SQL_CONFIG_FILE = "/Users/troelslund/git/11_CDIO_D3_2/11_CDIO_D3/sql.config";
+	private final String MYSQL_CONFIG_FILE = "mysql_mariadb.config";
+	private final String SQL_CONFIG_FILE = "sql.config";
 
 	/*
 	 * String objects to hold the database configuration information.
