ca425f5a9bdeedfca2a9466dec2114b4e61e5f16
InAbsencia <absentia@protonmail.ch>
2017-04-18 08:04:40 +0200

    Removed unnecessary project files.

--- 11_CDIO_D3/src/RESTResources/ApplicationConfig.java
+++ 11_CDIO_D3/src/RESTResources/ApplicationConfig.java
@@ -1,23 +1,12 @@
 package RESTResources;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
 
 /*
  * Application configuration class for the REST/JERSEY API.
  */
 @ApplicationPath("/api")
 public class ApplicationConfig extends Application {
-	
-	@GET
-	@Path("/test")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String Test() {
-		return "If you see this, REST should be working correctly!";
-	}
 
 }
